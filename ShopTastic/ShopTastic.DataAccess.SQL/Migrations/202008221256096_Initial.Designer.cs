9ed7a703d5fd5f4e64ec87d3b9fa9b67998bffa4
Lohitha Yerramsetty <lohitha.yerramsetty@gmail.com>
2020-08-22 15:14:32 +0200

    Added Basket

--- ShopTastic/ShopTastic.DataAccess.SQL/Migrations/202008221256096_Initial.Designer.cs
+++ ShopTastic/ShopTastic.DataAccess.SQL/Migrations/202008221256096_Initial.Designer.cs
@@ -7,13 +7,13 @@ namespace ShopTastic.DataAccess.SQL.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.2.0-61023")]
-    public sealed partial class initial : IMigrationMetadata
+    public sealed partial class Initial : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(initial));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(Initial));
         
         string IMigrationMetadata.Id
         {
-            get { return "202008211429117_initial"; }
+            get { return "202008221256096_Initial"; }
         }
         
         string IMigrationMetadata.Source
