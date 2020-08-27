2ead5a190f1d7eea56f5fa0b253790ab19dfe4aa
Ehtisham Zaheer <ehtisham_zaheer@hotmail.com>
2020-08-27 14:28:12 +0200

    changes in home--index--bootstrap classes

--- ShopTastic/ShopTastic.DataAccess.SQL/Migrations/202008260807012_initial.Designer.cs
+++ ShopTastic/ShopTastic.DataAccess.SQL/Migrations/202008260807012_initial.Designer.cs
@@ -7,13 +7,13 @@ namespace ShopTastic.DataAccess.SQL.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.2.0-61023")]
-    public sealed partial class intital : IMigrationMetadata
+    public sealed partial class initial : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(intital));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(initial));
         
         string IMigrationMetadata.Id
         {
-            get { return "202008251807218_intital"; }
+            get { return "202008260807012_initial"; }
         }
         
         string IMigrationMetadata.Source
--- ShopTastic/ShopTastic.Services/BasketService.cs
+++ ShopTastic/ShopTastic.Services/BasketService.cs
@@ -70,7 +70,7 @@ namespace ShopTastic.Services
         public void AddToBasket(HttpContextBase httpContext, string productId)
         {
             Basket basket = GetBasket(httpContext, true);
-            BasketItem item = basket.BasketItems.FirstOrDefault(i => i.Id == productId);
+            BasketItem item = basket.BasketItems.FirstOrDefault(i => i.ProductId == productId);
 
             if (item == null)
             {
